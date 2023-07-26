7fd4d43e2137add077fa0582f08e9bcb845789b4
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2023-07-26 09:21:18 +0900

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -212,46 +212,56 @@ export default createStore({
     ],
     communities: [
       {
+        id: uniqueId('card'),
         titleImg: require('@/assets/images/Larachat.jpg'),
         title: 'Code Newbie',
         describe: 'The most supportive community of programmers and people learning to code',
         profileImg: require('@/assets/images/Larachat.jpg'),
+        shopIcon: require('@/assets/images/communitiesIcon.jpg'),
         name: 'fonuded by saron yitbarek',
         snsIcon: ['github', 'slack', 'partreon'],
         isBookmark: false
       },
       {
+        id: uniqueId('card'),
         titleImg: require('@/assets/images/Larachat.jpg'),
         title: 'FEDs on Slack',
         describe: 'A Community of Top-notch Front End Developers on Slack',
         profileImg: require('@/assets/images/Larachat.jpg'),
+        shopIcon: require('@/assets/images/communitiesIcon.jpg'),
         name: 'Founded by Yogev Ahuvia',
         snsIcon: ['github'],
         isBookmark: false
       },
       {
+        id: uniqueId('card'),
         titleImg: require('@/assets/images/Larachat.jpg'),
         title: 'Free code camp',
         describe: 'Learn to code. Build projects. Earn certifications.Since 2015, 40,000 graduates have gotten jobs at tech companies including Google, Apple, Amazon, and Microsoft.',
         profileImg: require('@/assets/images/Larachat.jpg'),
+        shopIcon: require('@/assets/images/communitiesIcon.jpg'),
         name: 'Founded by Quincy Larson',
         snsIcon: ['slack', 'github'],
         isBookmark: false
       },
       {
+        id: uniqueId('card'),
         titleImg: require('@/assets/images/Larachat.jpg'),
         title: 'indie hackers',
         describe: 'Connect with developers who are sharing the strategies and revenue numbers behind their companies and side projects.',
         profileImg: require('@/assets/images/Larachat.jpg'),
+        shopIcon: require('@/assets/images/communitiesIcon.jpg'),
         name: 'Founded by courtland allen',
         snsIcon: ['partreon', 'github'],
         isBookmark: false
       },
       {
+        id: uniqueId('card'),
         titleImg: require('@/assets/images/Larachat.jpg'),
         title: 'Larachat',
         describe: "THE SLACK COMMUNITY FOR LARAVEL DEVELOPERS & MORE. Chat and share with over 31,800 worldwide members. We have live discussions. We're building the biggest PHP virtual user group in the world. Come join the fun.",
         profileImg: require('@/assets/images/Larachat.jpg'),
+        shopIcon: require('@/assets/images/communitiesIcon.jpg'),
         name: 'Founded by shawn mayzes',
         snsIcon: ['linkedin', 'dev-to'],
         isBookmark: false
@@ -275,6 +285,36 @@ export default createStore({
         state.bookmarkArr.splice(deleIdx, 1)
       }
     },
+    addBookmark (state, item) {
+      const nowState = item[1]
+      const addMark = nowState.find(el => el.id === item[0].id)
+      const hasItem = state.bookmarkArr.find(el => el.id === item[0].id)
+      const deleIdx = state.bookmarkArr.indexOf(hasItem)
+
+      // const addMark = state.communities.find(el => el.id === item.id)
+      // const hasItem = state.bookmarkArr.find(el => el.id === item)
+      // const deleIdx = state.bookmarkArr.indexOf(hasItem)
+
+      if (addMark.isBookmark === false) {
+        addMark.isBookmark = true
+        state.bookmarkArr.push(addMark)
+      } else {
+        addMark.isBookmark = false
+        state.bookmarkArr.splice(deleIdx, 1)
+      }
+    },
+    deleteBookmarkItem (state, item) {
+      const nowState = item[1]
+
+      const delItem = nowState.find(el => el.id === item[0].id)
+      const delIdx = nowState.indexOf(delItem)
+
+      const bookmarItem = state.bookmarkArr.find(el => el.id === item[0].id)
+      const bookmarIdx = state.bookmarkArr.indexOf(bookmarItem)
+
+      nowState[delIdx].isBookmark = false
+      state.bookmarkArr.splice(bookmarIdx, 1)
+    },
     clearBookmark (state) {
       for (let i = 0; i < state.Items.length; i++) {
         state.Items[i].isBookmark = false
@@ -283,25 +323,6 @@ export default createStore({
         state.communities[i].isBookmark = false
       }
       state.bookmarkArr = []
-    },
-    deleteBookmarkItem (state, deleteId) {
-      const delItem = state.bookmarkArr.find(el => el.id === deleteId)
-      const delIdx = state.bookmarkArr.indexOf(delItem)
-      delItem.isBookmark = false
-      state.bookmarkArr.splice(delIdx, 1)
-    },
-    addBookmarkArr (state, item) {
-      const addMark = state.communities.find(el => el === item)
-      const hasItem = state.bookmarkArr.find(el => el === item)
-      const deleIdx = state.bookmarkArr.indexOf(hasItem)
-
-      if (addMark.isBookmark === false) {
-        item.isBookmark = true
-        state.bookmarkArr.push(addMark)
-      } else {
-        item.isBookmark = false
-        state.bookmarkArr.splice(deleIdx, 1)
-      }
     }
   },
   actions: {
