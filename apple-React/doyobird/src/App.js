d35011537ab0422d9fbe28bc42e8cb2a4d2893eb
shinchaeyoun <jun29182877@gamil.com>
2023-08-22 16:06:07 +0900

    add

--- apple-React/doyobird/src/App.js
+++ apple-React/doyobird/src/App.js
@@ -19,7 +19,7 @@ function App() {
   let [done, setDone] = useState(false);
   let [time, setTime] = useState(timeString);
 
-  getTime()
+  // getTime()
   function getTime () {
     setInterval(() => {
       setTime(timeString);
