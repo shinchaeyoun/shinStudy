edb889c594a059f49229986788a825cbf63dc319
shinchaeyoun <jun29182877@gamil.com>
2023-09-04 13:38:00 +0900

    update;

--- vlpt-todo/src/App.js
+++ vlpt-todo/src/App.js
@@ -1,10 +1,9 @@
-/* esline-disable */
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
-import TodoItem from './components/TodoItem';
+import TodoCreate from './components/TodoCreate';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -16,12 +15,11 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <div>
-        <TodoTemplate>
-          <TodoHead />
-          <TodoList />
-        </TodoTemplate>
-      </div>
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
     </>
   );
 }
---
+++ vlpt-todo/src/TodoContext.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+import React, { useReducer } from 'react';
+
+const initialTodos = [
+  {
+    id: 1,
+    text: 'create project',
+    done: true
+  },
+  {
+    id: 2,
+    text: 'components styling',
+    done: true
+  },
+  {
+    id: 3,
+    text: 'Context make'
+  },
+  {
+    id: 4,
+    text: 'object 4',
+    done: false
+  }
+];
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return state.concat(action.todo);
+    case 'TOGGLE':
+      return state.map(todo =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case 'REMOVE':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      throw new Error(`Unhandled action type: ${active.type}`);
+  }
+};
+
+export function TodoProvider({children}) {
+  const [state, dispatch] = useReducer(todoReducer, initialTodos);
+  return children;
+}
