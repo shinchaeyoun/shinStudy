05a09f58868be63d1f1c2c63cc2130f241b88377
shinchaeyoun <jun29182877@gamil.com>
2023-09-08 15:30:57 +0900

    deldete

---
+++ appydev-react/src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import styled from 'styled-components';
+import './../../Styles/variables.scss';
+import './Nav.scss';
+
+import Main from './../../pages/Main/Main';
+
+function HeaderNav() {
+  return (
+    <nav>
+      nav
+    </nav>
+  )
+}
+
+export default HeaderNav;
---
+++ appydev-react/src/pages/Main/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main() {
+  return <>main</>;
+}
+
+export default Main;
