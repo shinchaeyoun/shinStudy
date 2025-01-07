ac3e29960edf3f9b4c5b01df5c4fbb19c0d11399
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2025-01-07 13:45:23 +0900

    'a'

--- make01/src/index.js
+++ make01/src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render();
